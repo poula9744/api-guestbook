574be0ce13b4a6de1b4bf8d3df80d377d29e5480
poula9744 <poula9744@naver.com>
2024-04-12 11:51:20 +0900

    55

--- src/main/java/com/javaex/controller/GuestbookController.java
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -21,7 +21,7 @@ public class GuestbookController {
 	private GuestbookService guestbookService;
 	
 	//리스트
-	//@ResponseBody --> Method가 GET == GetMapping
+	//@ResponseBody --> Method가 GET == GetMapping ㅎ
 	@GetMapping(value="/api/guests")
 	public List<GuestbookVo> list() {
 		System.out.println("GuestbookController.list()");
